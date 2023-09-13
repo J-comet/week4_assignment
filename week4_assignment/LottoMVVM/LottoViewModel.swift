93132d0b8d4695dddc2228f50041222e7d2ac29b
hyeseong <hyeseong7848@gmail.com>
2023-09-13 16:30:16 +0900

    로또 MVVM 으로 변경 진행중 - repository, model, observable, viewModel 추가

---
+++ week4_assignment/LottoMVVM/LottoViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  LottoViewModel.swift
+//  week4_assignment
+//
+//  Created by 장혜성 on 2023/09/13.
+//
+
+import Foundation
+
+class LottoViewModel {
+    
+    let numList: [Int] = Array(1...1100).reversed()
+    
+}
+
